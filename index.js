f6a1e929dc8b4c42a371306f750472a313c8b798
Dheeraj <dheerajsinghkushwaha20@gmail.com>
2026-03-28 15:40:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express =require ('express');
+
+const app =express();
+
+const port =3000
+
+app.get('/',(req,res)=>{
+    res.send('Hellow ')
+});
+app.get('/login',(req,res)=>{
+    res.send('<h1> please login at my web page<h1/>')
+});
+
+
+app.get('/dheeraj',(req,res)=>{
+    res.send('<h1> Hello Dheeraj Kumar Kaishe ho kya kar rhe ho meow ghop ghop ghop</h1>')
+
+})
+app.listen(process.env.PORT,()=>{
+    console.log(` Example app listening on port ${port}`);
+    
+});
